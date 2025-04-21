c36b2148f42dfdd0f6b6a3040a441605a39ca7f1
Scott Bedard <scottbedard90@gmail.com>
2025-04-20 19:52:33 -0500

    cleanup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,30 +1,5 @@
-import inject from '@rollup/plugin-inject'
-import topLevelAwait from 'vite-plugin-top-level-await'
-import wasm from 'vite-plugin-wasm'
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  nitro: {
-    experimental: {
-      wasm: true,
-    },
-  },
-  vite: {
-    build: {
-      target: 'esnext',
-      rollupOptions: {
-        plugins: [
-          inject({
-            exclude: ['**/*.wasm'],
-          })
-        ]
-      }
-    },
-    plugins: [
-      wasm(),
-      topLevelAwait(),
-    ],
-  },
 })
